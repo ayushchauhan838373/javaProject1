852b7a9e9afb1e693af4d4409f51f0421b3f6af0
ayushchauhan838373 <ayushchauhan.dev01@gmail.com>
2025-06-11 20:31:53 +0530

    Add files via upload

---
+++ ManagingEmployeeReports.java
@@ -0,0 +1,93 @@
+package managerOfOffice;
+
+public class ManagingEmployeeReports {
+	
+	String employeeName;
+	String positionInCompany;
+	String department;
+	int employeeNumber;
+	int checkIn_time;
+	int cheeckOut_time;
+	String projectOn_Working;
+	String phoneNumber;
+	int totalleavesIn_Month;
+	int totalExperience;
+	double performanceGrade;
+	
+	
+	
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getPositionInCompany() {
+		return positionInCompany;
+	}
+	public void setPositionInCompany(String positionInCompany) {
+		this.positionInCompany = positionInCompany;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+	public int getCheckIn_time() {
+		return checkIn_time;
+	}
+	public void setCheckIn_time(int checkIn_time) {
+		this.checkIn_time = checkIn_time;
+	}
+	public int getCheeckOut_time() {
+		return cheeckOut_time;
+	}
+	public void setCheeckOut_time(int cheeckOut_time) {
+		this.cheeckOut_time = cheeckOut_time;
+	}
+	public String getProjectOn_Working() {
+		return projectOn_Working;
+	}
+	public void setProjectOn_Working(String projectOn_Working) {
+		this.projectOn_Working = projectOn_Working;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getTotalleavesIn_Month() {
+		return totalleavesIn_Month;
+	}
+	public void setTotalleavesIn_Month(int totalleavesIn_Month) {
+		this.totalleavesIn_Month = totalleavesIn_Month;
+	}
+	public int getTotalExperience() {
+		return totalExperience;
+	}
+	public void setTotalExperience(int totalExperience) {
+		this.totalExperience = totalExperience;
+	}
+	public double getPerformanceGrade() {
+		return performanceGrade;
+	}
+	public void setPerformanceGrade(double performanceGrade) {
+		this.performanceGrade = performanceGrade;
+	}
+	
+	
+	
+	
+	
+
+
+}
